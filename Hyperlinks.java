5fc98a76577ac16145df6776ea65c55f03ac7464
Adarsh-Ashesh <100584197+Adarsh-Ashesh@users.noreply.github.com>
2023-09-19 15:06:38 +0530

    Add files via upload

--- Hyperlinks.java
+++ Hyperlinks.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hyperlinks {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
@@ -20,6 +20,13 @@ public class Hyperlinks {
 		java.util.List<WebElement> links = driver.findElements(By.tagName("a"));
 		
 		System.out.println("Number of hyperlinks present are : " + links.size());
+		
+        java.util.List<WebElement> imgLinks = driver.findElements(By.xpath("//div[@class='style__WidgetContainerBody-sc-lnhrs7-4 YeNog']/div/a"));
+		
+		System.out.println("Number of image links present are : " + imgLinks.size());
+		
+		
+		}
 	}
 
-}
+
